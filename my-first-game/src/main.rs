144b2348feddcef6515f56854da050d3ce6bfcd8
谢祺鹏 <15949180087@163.com>
2025-04-09 22:18:27 +0800

    Uncomment background clearing in main game loop

--- my-first-game/src/main.rs
+++ my-first-game/src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("My First Game")]
 async fn main() {
     loop {
-        // clear_background(DARKGREEN);
+        clear_background(DARKGREEN);
         next_frame().await;
     }
 }
