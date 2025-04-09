e9cfeca323bb2711662331dd8aedd39b6bf76d31
谢祺鹏 <15949180087@163.com>
2025-04-09 22:06:42 +0800

    Implement initial game setup with macroquad and add deployment workflow

--- my-first-game/src/main.rs
+++ my-first-game/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("My First Game")]
+async fn main() {
+    loop {
+        // clear_background(DARKGREEN);
+        next_frame().await;
+    }
 }
