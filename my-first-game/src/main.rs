94a94c29460de89b04f0cfd39cf0fd938b2fe531
nargacugaa <15949180087@163.com>
2025-04-10 11:43:08 +0800

    Refactor movement logic to use delta time for smoother player control

--- my-first-game/src/main.rs
+++ my-first-game/src/main.rs
@@ -1,28 +1,45 @@
+use std::ops::Add;
+
 use macroquad::prelude::*;
 
 #[macroquad::main("My First Game")]
 async fn main() {
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
+    const MOVEMENT_SPEED: f32 = 400.0;
+
+    let window_screen_width = screen_width();
+    let window_screen_height = screen_height();
+
+    let mut x = window_screen_width / 2.0;
+    let mut y = window_screen_height / 2.0;
+
+    // 使用 color_u8! 宏创建颜色
+    let color = color_u8!(1, 55, 99, 99);
+    println!("{:?}", color);
 
     loop {
         clear_background(DARKBROWN);
 
+        let delta_time = get_frame_time();
+        let move_frame_speed = MOVEMENT_SPEED * delta_time;
+
         if is_key_down(KeyCode::Right) {
-            x += 1.0;
+            x += move_frame_speed;
         }
         if is_key_down(KeyCode::Left) {
-            x -= 1.5;
+            x -= move_frame_speed;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 2.0;
+            y -= move_frame_speed;
         }
         if is_key_down(KeyCode::Down) {
-            y += 1.0;
+            y += move_frame_speed;
         }
 
-        // draw_circle(x, y, 16.0, color_u8!(1.00, 0.43, 0.76, 1.00));
+        draw_circle(x.add(1f32), y.add(1f32), 32.0, color);
         draw_circle(x, y, 16.0, BLACK);
+
+        x = clamp(x, 0f32, window_screen_width);
+        y = clamp(y, 0f32, window_screen_height);
 
         next_frame().await;
     }
